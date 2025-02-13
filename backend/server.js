e33908a70c1d456d357251668270f29d52efaf4a
Stefaan <safwbdev@gmail.com>
2025-02-13 10:27:46 +0800

    adjust server js

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,18 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cookieParser());
 app.use('/api/users', userRoutes);
 
-app.get('/', (req, res) => res.send('Server is running!'));
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, 'frontend/dist')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
+} else {
+    app.get('/', (req, res) => res.send('Server is running!'));
+
+}
 
 app.use(notFound);
 app.use(errorHandler);
